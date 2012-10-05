09ae0b58771c753c3ad7be666475fd27672f58b7
mail.jeffbernard <mail.jeffbernard@gmail.com>
2012-10-05 05:03:08 +0000

    +AlphaBetaAI compiles

--- src/alphabetaai.cpp
+++ src/alphabetaai.cpp
@@ -1,6 +1,7 @@
 #include "alphabetaai.h"
 #include <limits>
 #include <cmath>
+#include <cstring>
 
 /**
  * Inits a new AI
@@ -8,7 +9,15 @@
  *@param bh board height
  */
 AlphaBetaAI::AlphaBetaAI(int bw, int bh): AI(bw, bh) {
+    _board = new int[bw*bh];
+    bestMove = 0;
+}
 
+/**
+ * Frees memory
+ */
+AlphaBetaAI::~AlphaBetaAI() {
+    delete _board;
 }
 
 /**
@@ -20,23 +29,22 @@ AlphaBetaAI::AlphaBetaAI(int bw, int bh): AI(bw, bh) {
  *@return my move
  */
 int AlphaBetaAI::makeMove(int* board, int score, int opponentScore, int pointsRemaining) {
-    int x = 0;
-    int y = 0;
-
     int depth = 5; //needs to be a time limit?..Iterative Deepening
     int inf = numeric_limits<int>::max();
 
     //Create copy of the board for use in search tree
-    _board = new int[boardWidth*boardHeight];
-    for (int i=0; i < boardWidth*boardHeight; i++)
+    //_board = new int[boardWidth*boardHeight];
+    /*for (int i=0; i < boardWidth*boardHeight; i++)
     {
         _board[i] = board[i];
-    }
+    }*/
+    // found a probably faster copy function than iterating
+    memcpy((void *)_board, (void *)board, sizeof(int)*boardWidth*boardHeight);
 
     int maxPlayer = 1;
-    int val = alpha_beta( _board, depth, -inf, inf, maxPlayer); //calls the alpha_beta search process
+    alpha_beta( _board, depth, -inf, inf, maxPlayer, score, opponentScore, pointsRemaining); //calls the alpha_beta search process
 
-    return val;
+    return bestMove;
 }
 
 /**
@@ -46,11 +54,11 @@ int AlphaBetaAI::makeMove(int* board, int score, int opponentScore, int pointsRe
  *@param beta the minimax value for beta
  *@return x y coordinates for move
  */
-int AlphaBetaAI::alpha_beta(int* _board, int depth, int alpha, int beta, int maxPlayer)
+float AlphaBetaAI::alpha_beta(int* _board, int depth, float alpha, float beta, int maxPlayer, int myScore, int otherScore, int pointsRemaining)
 {
-    if (depth == 0) // or is a terminal node?
+    if (depth == 0 || pointsRemaining == 0) // or is a terminal node?
     {
-        return evaluate(_board);
+        return evaluate(_board, myScore, otherScore, maxPlayer);
     }
 
     vector<int> legalMoves;
@@ -58,25 +66,58 @@ int AlphaBetaAI::alpha_beta(int* _board, int depth, int alpha, int beta, int max
 
     while (!legalMoves.empty()) //for each child node of max and min player
     {
-        maxPlayer = makeNextMove(_board, legalMoves, maxPlayer); //changes betweens turn unless box completed
+        int myPoints = myScore;
+        int otherPoints = otherScore;
+        int pointsLeft = pointsRemaining;
+        int moves = 0;
+        do {
+            moves++;
+            /// TODO: legalMoves[legalMoves.size()-moves]  is where we put move ordering
+        } while (moves < legalMoves.size() && makeNextMove(_board, legalMoves[legalMoves.size()-moves], maxPlayer, myPoints, otherPoints, pointsLeft));
+        if (moves == legalMoves.size()) {
+            moves--;
+        }
+        //maxPlayer = makeNextMove(_board, legalMoves, maxPlayer); //changes betweens turn unless box completed
         if (maxPlayer)
         {
-            alpha = max(alpha, alpha_beta(_board, depth-1,alpha, beta, maxPlayer));
-            if (beta <= alpha)
+            float eval = alpha_beta(_board, depth-1, alpha, beta, !maxPlayer, myPoints, otherPoints, pointsLeft);
+            //alpha = max(alpha, eval);
+            if (eval > alpha) {
+                alpha = eval;
+                if (depth == 5) {
+                    bestMove = legalMoves[legalMoves.size()-1];
+                }
+            }
+            if (beta <= alpha) {
                 break; //beta cut-off
+            }
         }
         else
         {
-            beta = min(beta, alpha_beta(_board, depth-1, alpha, beta, maxPlayer));
-            if (beta <= alpha)
+            float eval = alpha_beta(_board, depth-1, alpha, beta, !maxPlayer, myPoints, otherPoints, pointsLeft);
+//            beta = min(beta, );
+            if (eval < beta) {
+                beta = eval;
+            }
+            if (beta <= alpha) {
                 break; //alpha cut-off
+            }
         }
+        while (moves > 0) {
+            /// TODO: this needs to be changed when move ordering is implemented
+            unmakeLastMove(_board, legalMoves[legalMoves.size()-moves]);
+            moves--;
+        }
+        legalMoves.pop_back();
     }//end while loop of all legalMoves
-
-    if(maxPlayer)
+    if (maxPlayer) {
         return alpha;
-    else
-        return beta;
+    }
+    return beta;
+}
+
+void AlphaBetaAI::unmakeLastMove(int* _board, int move) {
+    _board[move] = BOARD_EMPTY;
 }
 
 /**
@@ -88,8 +129,9 @@ int AlphaBetaAI::alpha_beta(int* _board, int depth, int alpha, int beta, int max
  {
      for( int i=0; i<boardWidth*boardHeight; i++)
      {
-         if(board[i] == 0)
+         if(board[i] == BOARD_EMPTY) {
             v.push_back(i);
+         }
      }
  }
 
@@ -98,18 +140,28 @@ int AlphaBetaAI::alpha_beta(int* _board, int depth, int alpha, int beta, int max
  * @param _board copy board for search
  * @param moves vector reference to legalMoves
  */
-int AlphaBetaAI::makeNextMove(int *_board, vector<int> &moves, int maxPlayer)
-{
-    /*
-    int pointsRem = _board.getPointsRemaining();
+int AlphaBetaAI::makeNextMove(int *_board, int move, int maxPlayer, int& myScore, int& otherScore, int& pointsRemaining) {
+    _board[move] = BOARD_LINE;
+    int squares = Board::squaresMade(_board, move);
+    if (squares) {
+        pointsRemaining -= squares;
+        if (maxPlayer) {
+            myScore += squares;
+        } else {
+            otherScore += squares;
+        }
+    }
+    return squares;
+
+    /*int pointsRem = _board.getPointsRemaining();
     int nextMove = moves.back();
     _board.playBoardCoord(nextMove);
 
     if(pointsRem != _board.getPointsRemaining()) //if change in score, then box was completed and maxPlayer gets to go again
         return maxPlayer;
 
-    return !maxPlayer; //no change in score, so minPlayers turn
-
+    return !maxPlayer;*/ //no change in score, so minPlayers turn
+ /*
     _board[nextMove] = BOARD_LINE;
     if (squaresMade(board, nextMove) > 0)
     {
@@ -120,7 +172,7 @@ int AlphaBetaAI::makeNextMove(int *_board, vector<int> &moves, int maxPlayer)
 }
 
 /*
-/**
+/ **
  * Undoes the previous move on search board
  *  and deletes the move from legalMoves
  * @param _board copy board for search
@@ -140,29 +192,46 @@ void unMakeMove(int *_board, vector<int> &v)
  *@param _board pointer to the current state of the board COPY used to create tree
  *@return eval the value of the function
  */
-int AlphaBetaAI::evaluate(int* _board)
+float AlphaBetaAI::evaluate(int* board, int myScore, int otherScore, bool myMove)
 {
-    int scoreA = 0;
-    int scoreB = 0;
-    int line3 = 0;
-    int line2 = 0;
+    int s3 = 0;
+    int s2 = 0;
+    int size = boardWidth*boardHeight;
 
-    for (int i=1; i<boardWidth*boardHeight; i++)
-    {
-        if(_board[i] == BOARD_P1_SQUARE)
-            scoreA++;
-        else if (_board[i] == BOARD_P2_SQUARE)
-            scoreB++;
-
+    // first square starts at 1x1 = width+2
+    for (int i = boardWidth+2; i < size; i+=2) {
+        if (board[i] == BOARD_DOT) { // advance to next row
+            i += boardWidth+1;
+            if (i >= size) {
+                break;
+            }
+        }
+        // check if s2 or s3
+        int sides = 0;
+        if (board[i-1] == BOARD_LINE) {
+            sides++;
+        }
+        if (board[i+1] == BOARD_LINE) {
+            sides++;
+        }
+        if (board[i+boardWidth] == BOARD_LINE) {
+            sides++;
+        }
+        if (board[i-boardWidth] == BOARD_LINE) {
+            sides++;
+        }
+        if (sides == 2) {
+            s2++;
+        } else if (sides == 3) {
+            s3++;
+        }
     }
 
-
-    int eval = 2*scoreA - 2*scoreB + .75*line3 - .5*line2;
-    return eval;
+    return 2*myScore-2*otherScore+(0.75f*s3-0.5*s2)*(myMove?1:-1);
 }
 
 /*
-/**
+/ **
  * Checks how many squares are made by the line
  *@param board the game board
  *@param line the line that was placed
